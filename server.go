cdcfe0ccc093bd03cf2daad140397868a7f59104
Weston Platter <westonplatter@gmail.com>
2014-10-01 01:12:35 -0600

    todos CRUD work. Created and Index work.

--- server.go
+++ server.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
 	"regexp"
 )
 
 // net/http based router
-
 type route struct {
 	pattern *regexp.Regexp
 	verb    string
@@ -38,21 +40,30 @@ func (h *RegexpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
-// store "context" values and connections in the server struct
 // todo "Object"
 type Todo struct {
-	Id           int
-	Title        string
-	Category     string
-	Dt_created   int64
-	Dt_completed int64
-	State        string
+	Id         int    `json:"Id"`
+	Title      string `json:"Title"`
+	Category   string `json:"Category"`
+	Dt_created int64  `json:"int64"`
+	Dt_updated int64  `json:"int64"`
+	State      string `json:"State"`
 }
 
-type Server struct{}
+// store "context" values and connections in the server struct
+type Server struct {
+	db *sql.DB
+}
 
 func main() {
-	server := &Server{}
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang_todo_dev")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.SetMaxIdleConns(100)
+	defer db.Close()
+
+	server := &Server{db: db}
 
 	reHandler := new(RegexpHandler)
 
@@ -69,6 +80,8 @@ func main() {
 	http.ListenAndServe(":3000", reHandler)
 }
 
+// simple HTML/JS/CSS pages
+
 func (s *Server) homepage(res http.ResponseWriter, req *http.Request) {
 	http.ServeFile(res, req, "./index.html")
 }
@@ -77,23 +90,48 @@ func (s *Server) assets(res http.ResponseWriter, req *http.Request) {
 	http.ServeFile(res, req, req.URL.Path[1:])
 }
 
+// Todo CRUD
+
 func (s *Server) todoIndex(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("Array of todo json")
+	var todos []*Todo
+
+	rows, err := s.db.Query("SELECT Id, Title, Category, Dt_created, Dt_updated, State FROM Todo")
+	error_check(res, err)
+	for rows.Next() {
+		todo := &Todo{}
+		rows.Scan(&todo.Id, &todo.Title, &todo.Category, &todo.Dt_created, &todo.Dt_updated, &todo.State)
+		todos = append(todos, todo)
+	}
+	rows.Close()
+
+	jsonResponse(res, todos)
 }
 
 func (s *Server) todoCreate(res http.ResponseWriter, req *http.Request) {
 	todo := &Todo{}
+
 	decoder := json.NewDecoder(req.Body)
 	err := decoder.Decode(&todo)
-
 	if err != nil {
 		fmt.Println("ERROR decoding JSON - ", err)
 		return
 	}
 
-	fmt.Println(todo)
+	result, err := s.db.Exec("INSERT INTO Todo(Title, Category, State, Dt_created) VALUES(?, ?, ?, ?)", todo.Title, todo.Category, todo.State, todo.Dt_created)
 
-	fmt.Println("Created Todo. Send back todo json")
+	if err != nil {
+		fmt.Println("ERROR saving to db - ", err)
+	}
+
+	newId64, err := result.LastInsertId()
+	newId := int(newId64)
+	todo = &Todo{Id: newId}
+
+	s.db.
+		QueryRow("SELECT State, Title, Category, Dt_created, Dt_updated FROM Todo WHERE Id=?", todo.Id).
+		Scan(&todo.State, &todo.Title, &todo.Category, &todo.Dt_created, &todo.Dt_updated)
+
+	jsonResponse(res, todo)
 }
 
 func (s *Server) todoShow(res http.ResponseWriter, req *http.Request) {
@@ -106,4 +144,24 @@ func (s *Server) todoUpdate(res http.ResponseWriter, req *http.Request) {
 
 func (s *Server) todoDelete(res http.ResponseWriter, req *http.Request) {
 	fmt.Println("Deleted todo. Render todo json")
+}
+
+func jsonResponse(res http.ResponseWriter, data interface{}) {
+
+	res.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	payload, err := json.Marshal(data)
+	if error_check(res, err) {
+		return
+	}
+
+	fmt.Fprintf(res, string(payload))
+}
+
+func error_check(res http.ResponseWriter, err error) bool {
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return true
+	}
+	return false
 }
